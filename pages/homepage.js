a437396bdd03a45bf8145c99b063cffed544fcfd
beebisme <100251496+beebisme@users.noreply.github.com>
2022-05-01 06:08:01 +0700

    add new homepage

--- pages/homepage.js
+++ pages/homepage.js
@@ -5,20 +5,25 @@ import Link from "next/dist/client/link";
 
 export default function HomePage() {
     return (
-        <>
-            <Navbar />
-            <div className="container flex flex-col-reverse md:flex-row justify-center my-20 md:my-20">
-                <div className="flex flex-col self-center">
-                    <div className="mx-10 md:mx-20 text-5xl font-bold text-[#0A192F] leading-tight">Learn Microsoft Office Easily</div>
-                    <div className="flex flex-col lg:flex-row mx-10 md:mx-20 my-10">
-                        <div className="mr-4 mb-4 lg:mb-0 w-48 text-center border-2 border-[#133C7C] hover:bg-[#133C7C] rounded p-2 font-semibold hover:text-white"><Link href="/microsoft-word/word">Microsoft Word</Link></div>
-                        <div className="w-48 text-center border-2 border-[#133C7C] hover:bg-[#133C7C] rounded p-2 font-semibold hover:text-white"><Link href="/powerpoint">Microsoft Powerpoint</Link></div>
-                    </div>
-                </div>
-                <div className="flex mx-2 flex justify-center">
-                    <img src="/assets/developer.svg" alt="powerpoint" className="w-3/4 mb-10" />
-                </div>
+        <div className="overflow-hidden bg-black flex flex-col justify-center items-center">
+            <div className="relative h-screen w-screen opacity-60 -translate-y-10 scale-y-100 -z-2 blur-sm">
+                <video autoPlay loop muted>
+                    <source src="/assets/tim13.mp4" type="video/mp4" />
+                </video>
             </div>
-        </>
+            <div className="absolute top-0 h-screen w-screen z-2 bg-[#0A192F] opacity-50" />
+
+            <div className="absolute z-3">
+                <div className=" text-white font-bold text-6xl text-center">
+                    LEARN MICROSOFT OFFICE EASILY
+                </div>
+                <div className="flex flex-row mt-10 justify-center">
+                    <div className="mr-4 mb-4 lg:mb-0 w-48 text-center border-2 border-white text-white drop-shadow-lg shadow-[#133C7C] rounded-lg p-4 font-semibold hover:bg-white hover:text-[#0A192F]"><Link href="/microsoft-word/word">Microsoft Word</Link></div>
+                    <div className="w-58 ml-4 text-center border-2 border-white text-white rounded-lg p-4 font-semibold hover:bg-white hover:text-[#0A192F]"><Link href="/powerpoint">Microsoft Powerpoint</Link></div>
+                </div>
+
+            </div>
+        </div>
+
     )
 }
