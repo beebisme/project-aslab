e566669f77b0a56b59b50dce3ec7176653ead23e
beebisme <100251496+beebisme@users.noreply.github.com>
2022-05-10 21:09:29 +0700

    nambah materi

--- components/listMateriPP.js
+++ components/listMateriPP.js
@@ -7,7 +7,7 @@ export default function ListMateriPP() {
             <h1 className="text-xl font-semibold">Daftar Materi Microsoft Powerpoint</h1>
             <div className="container list-decimal mt-2">
                 <li className="text-blue-500 hover:underline text-base md:w-96 mb-2 capitalize"><Link href="/microsoft-powerpoint/cara-menggunakan-pp-di-pc">Cara Menggunakan Powerpoint Di PC/Laptop</Link></li>
-                <li className="text-blue-500 hover:underline text-base md:w-96 mb-2"><Link href="/microsoft-powerpoint/fungsi-tool-">Fungsi Tools Group Font Pada PowerPoint</Link></li>
+                <li className="text-blue-500 hover:underline text-base md:w-96 mb-2"><Link href="/microsoft-powerpoint/fungsi-tool-group-font">Fungsi Tools Group Font Pada PowerPoint</Link></li>
                 <li className="text-blue-500 hover:underline text-base md:w-96 mb-2"><Link href="/microsoft-powerpoint/cara-menambahkan-dan-memformat-teks">Cara Menambahkan dan Memformat Teks Pada Powerpoint</Link></li>
                 <li className="text-blue-500 hover:underline text-base md:w-96 mb-2"><Link href="/microsoft-word/mencari-kata">Mencari Kata di Word dengan Find</Link></li>
                 <li className="text-blue-500 hover:underline text-base md:w-96 mb-2"><Link href="/microsoft-word/replace">Mengganti Kata di MS Word dengan Replace secara cepat</Link></li>
