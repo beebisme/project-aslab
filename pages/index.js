f0683a58f74e8c7c401d34dcef0e18c917e0965b
beebisme <100251496+beebisme@users.noreply.github.com>
2022-05-15 17:01:03 +0700

    homepage

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,8 @@
 import Head from 'next/head'
 import "tailwindcss/tailwind.css"
 import HomePage from './homepage'
-import ClipLoader from "react-spinners/PropagateLoader";
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 export default function Home() {
-  const [loading, setloading] = useState(false);
-
-  useEffect(() => {
-    setloading(true)
-    setTimeout(() => {
-      setloading(false)
-    }, 3000);
-  }, []);
-
   return (
     <div className="container">
       <Head>
@@ -23,31 +11,13 @@ export default function Home() {
       </Head>
 
       <main>
-        {
-          loading ?
-
-            <ClipLoader
-              size={20}
-              color={"#21B7DE"}
-              loading={loading}
-            />
-
-            :
-
-            setTimeout(() => {
-              <HomePage />
-            }, 500)
-        }
+        <HomePage />
       </main>
 
       <style jsx>{`
         .container {
           min-height: 100vh;
-          background: #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          width: 100%;
+          background: #eaeaea
         }
       `}</style>
     </div>
