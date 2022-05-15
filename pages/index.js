aeb580cd526cfef5cd2bbb7d0f45c623060f0461
beebisme <100251496+beebisme@users.noreply.github.com>
2022-05-15 16:23:48 +0700

    add spinner

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,20 @@
 import Head from 'next/head'
 import "tailwindcss/tailwind.css"
 import HomePage from './homepage'
+import ClipLoader from "react-spinners/PropagateLoader";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Home() {
+  const [loading, setloading] = useState(false);
+
+  useEffect(() => {
+    setloading(true)
+    setTimeout(() => {
+      setloading(false)
+    }, 3000);
+  }, []);
+
   return (
     <div className="container">
       <Head>
@@ -11,13 +23,29 @@ export default function Home() {
       </Head>
 
       <main>
-        <HomePage />
+        {
+          loading ?
+
+            <ClipLoader
+              size={20}
+              color={"#21B7DE"}
+              loading={loading}
+            />
+
+            :
+
+            <HomePage />
+        }
       </main>
 
       <style jsx>{`
         .container {
           min-height: 100vh;
-          background: #eaeaea
+          background: #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
         }
       `}</style>
     </div>
