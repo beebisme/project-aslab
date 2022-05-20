47692a38564d142903c6684306d9be3077bf63f1
beebisme <100251496+beebisme@users.noreply.github.com>
2022-05-20 15:46:39 +0700

    fix bug

--- pages/microsoft-powerpoint/menambahkan-slide.js
+++ pages/microsoft-powerpoint/menambahkan-slide.js
@@ -11,9 +11,8 @@ export default function Word() {
 
             {/* materi */}
             <div className="container flex flex-col lg:flex-row">
-                <div className="text-center w-11/12 lg:w-8/12 ml-10 lg:ml-20 my-6">
-                    <h1 className="font-bold text-2xl mb-4 mr-10 lg:mr-0 capitalize">Cara Menambahkan Slide Pada PowerPoint</h1>
-                    <p>(ini video)</p>
+                <div className="text-center w-11/12 lg:w-8/12 ml-10 my-6 px-10">
+                    <h1 className="font-bold text-2xl mb-4 mr-10 lg:mr-auto capitalize">Cara Menambahkan Slide Pada PowerPoint</h1>
                     <p className="text-justify mr-10 lg:mr-0 leading-normal">Setelah memilih lembar kerja presentasi, anda bisa membuat slide baru dengan memilih bagian New Slide pada menu seperti video di atas. Kemudian, akan muncul berbagai macam tipe slide. Pilih slide sesuai kebutuhan presentasi anda.  Untuk lebih jelasnya berikut akan dibahas satu-persatu fungsi pada menu - menunya :</p>
                     <div className="leading-relaxed pr-10 text-left mb-4">
                         <li><b> New Slide</b> digunakan untuk menambahkan slide baru yang akan kita buat.</li>
@@ -21,16 +20,10 @@ export default function Word() {
                         <li><b> Reset</b> digunakan untuk mengembalikan format kesemula atau default.</li>
                         <li><b> Section</b> digunakan mengelompokkan slide - slide yang memiliki topik yang sama.</li>
                     </div>
-
-                    <div className="w-10/12 my-10">
-                        <video controls>
-                            <source src="/assets/video/powerpoint/8.mp4" type="video/mp4" />
-                        </video>
-                    </div>
                 </div>
 
                 {/* list materi */}
-                <div className="mr-10 lg:mr-20 ml-10 my-8">
+                <div className="my-8 flex justify-center px-10">
                     <ListMateriPP />
                 </div>
             </div>
