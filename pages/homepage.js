3843e0260acd027b8bb83f9a42c35f42e4612cb5
beebisme <100251496+beebisme@users.noreply.github.com>
2022-08-27 18:03:21 +0700

    redesigning the homepage

--- pages/homepage.js
+++ pages/homepage.js
@@ -1,26 +1,22 @@
-import "tailwindcss/tailwind.css"
+import "tailwindcss/tailwind.css";
 import Link from "next/dist/client/link";
 
-
 export default function HomePage() {
-    return (
-        <>
-            <div className="overflow-hidden bg-black w-screen flex flex-col justify-center items-center">
-                <div className="relative translate-y-10 lg:-translate-y-10 h-screen w-screen opacity-60 scale-y-100 -z-1 blur-sm m-auto">
-                    <video autoPlay loop muted>
-                        <source src="/assets/tim13.mp4" type="video/mp4" />
-                    </video>
-                </div>
-                <div className="absolute z-3">
-                    <div className="text-white px-4 font-sans font-bold text-4xl lg:text-6xl text-center">
-                        LEARN MICROSOFT OFFICE EASILY
-                    </div>
-                    <div className="flex flex-col lg:flex-row mt-10 justify-center items-center">
-                        <button className=" w-max mb-4 lg:mb-0 text-center border-2 border-white text-white drop-shadow-lg shadow-[#133C7C] rounded-lg py-2 md:py-4 px-8 md:px-10 font-semibold hover:bg-white hover:text-[#0A192F]"><Link href="/word">Microsoft Word</Link></button>
-                        <button className="w-max lg:ml-4 text-center border-2 border-white text-white rounded-lg py-2 md:py-4 px-4 md:px-10 font-semibold hover:bg-white hover:text-[#0A192F]"><Link href="/powerpoint">Microsoft Powerpoint</Link></button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="overflow-hidden bg-gradient-to-tr from-slate-100 to-slate-400 w-screen h-screen flex flex-col justify-center items-center">
+        <div className="text-[#0A192F] px-4 font-sans font-bold text-4xl lg:text-6xl text-center">
+          LEARN MICROSOFT OFFICE EASILY
+        </div>
+        <div className="flex flex-row mt-10 justify-center items-center">
+          <button className="w-40 mr-4 text-center bg-[#00A4EF] text-white drop-shadow-lg rounded-lg py-4 px-8 font-semibold hover:bg-[#214479] hover:text-white">
+            <Link href="/word">Word</Link>
+          </button>
+          <button className="w-40 text-center bg-[#ED6C47] text-white drop-shadow-lg rounded-lg py-4 px-8 font-semibold hover:bg-[#C13B1B] hover:text-white">
+            <Link href="/powerpoint">Powerpoint</Link>
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
